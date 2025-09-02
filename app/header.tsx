fae1974f7c97f2e737e1b03a87ac72bae58420d4
Sergei Fedotov <fsa@tavda.info>
2025-09-02 18:58:23 +0700

    Все кнопки на сайте теперь иконки

--- app/header.tsx
+++ app/header.tsx
@@ -1,16 +1,15 @@
 import { NavLink } from "react-router"
-import { faQrcode } from '@fortawesome/free-solid-svg-icons';
-import { faHome } from '@fortawesome/free-regular-svg-icons';
+import { faHome, faCircleInfo, faQrcode, faRightToBracket } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Header = () => {
     return (
         <header>
             <ul className="header-menu">
-                <li><NavLink to="/"><FontAwesomeIcon icon={faHome} /></NavLink></li>
-                <li><NavLink to="/scanner"><FontAwesomeIcon icon={faQrcode} /></NavLink></li>
-                <li><NavLink to="/about">Инфо</NavLink></li>
-                <li><NavLink to="/login">Вход</NavLink></li>
+                <li><NavLink to="/"><FontAwesomeIcon icon={faHome} title="Домашняя страница" /></NavLink></li>
+                <li><NavLink to="/scanner"><FontAwesomeIcon icon={faQrcode} title="Сканнер"/></NavLink></li>
+                <li><NavLink to="/about"><FontAwesomeIcon icon={faCircleInfo} title="О сайте" /></NavLink></li>
+                <li><NavLink to="/login"><FontAwesomeIcon icon={faRightToBracket} title="Вход/Выход"/></NavLink></li>
             </ul>
         </header>
     )
