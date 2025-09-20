3e20494a855857de7c2ad1baca8f07efe8c767bf
Sergei Fedotov <fsa@tavda.info>
2025-09-21 02:06:48 +0700

    Изменён формат файла исходного кода, уменьшен размер текста на странице сериалов

--- app/components/MovieList.tsx
+++ app/components/MovieList.tsx
@@ -4,56 +4,56 @@ import { Link } from "react-router";
 import { api } from "~/api/client";
 
 interface Movie {
-    id: number,
-    name: string,
-    url: string
+  id: number,
+  name: string,
+  url: string
 }
 
 const MovieList = () => {
-    const [movieList, setMovieList] = useState<Movie[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+  const [movieList, setMovieList] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        setLoading(true);
-        api.post<Movie[]>('/movie',
-            {}
-        ).then((response) => {
-            setMovieList(response.data);
-            setError(null);
-        }).catch((error) => {
-            setMovieList([]);
-            setError('Ошибка при загрузке списка сериалов: ' + error);
-        }).finally(() => {
-            setLoading(false);
-        });
-    }, []);
+  useEffect(() => {
+    setLoading(true);
+    api.post<Movie[]>('/movie',
+      {}
+    ).then((response) => {
+      setMovieList(response.data);
+      setError(null);
+    }).catch((error) => {
+      setMovieList([]);
+      setError('Ошибка при загрузке списка сериалов: ' + error);
+    }).finally(() => {
+      setLoading(false);
+    });
+  }, []);
 
-    return (
-        <>
-            <Card>
-                <CardContent>
-                    <Typography variant="h1" component="h1" gutterBottom>
-                        Сериалы
-                    </Typography>
-                    <List>
-                        {movieList && movieList.map((movie) => (
-                            <ListItemButton key={movie.id} component={Link} to={`/movie/${movie.url}`}>
-                                <ListItemAvatar>
-                                    <Avatar alt={movie.name}>
-                                        {movie.name[0]}
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText primary={movie.name} secondary={`ID: ${movie.url}`} />
-                            </ListItemButton>
-                        ))}
-                        {loading && <CircularProgress />}
-                        {error && <Alert severity="error">{error}</Alert>}
-                    </List>
-                </CardContent>
-            </Card>
-        </>
-    );
+  return (
+    <>
+      <Card>
+        <CardContent>
+          <Typography gutterBottom>
+            Сериалы
+          </Typography>
+          <List>
+            {movieList && movieList.map((movie) => (
+              <ListItemButton key={movie.id} component={Link} to={`/movie/${movie.url}`}>
+                <ListItemAvatar>
+                  <Avatar alt={movie.name}>
+                    {movie.name[0]}
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={movie.name} secondary={`ID: ${movie.url}`} />
+              </ListItemButton>
+            ))}
+            {loading && <CircularProgress />}
+            {error && <Alert severity="error">{error}</Alert>}
+          </List>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
 
 export default MovieList;
