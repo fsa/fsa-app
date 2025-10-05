4d1f7411996f337f17c9fb90387b20c41f36db0b
Sergei Fedotov <fsa@tavda.info>
2025-10-06 04:16:21 +0700

    Сканирование всех форматов

--- app/components/QrScanner.tsx
+++ app/components/QrScanner.tsx
@@ -30,7 +30,29 @@ function QrScanner() {
   return (
     <>
       <div className='qr-scanner'>
-        <Scanner onScan={onScanCode} />
+        <Scanner onScan={onScanCode} formats={[
+          'aztec',
+          'code_128',
+          'code_39',
+          'code_93',
+          'codabar',
+          'databar',
+          'databar_expanded',
+          'data_matrix',
+          'dx_film_edge',
+          'ean_13',
+          'ean_8',
+          'itf',
+          'maxi_code',
+          'micro_qr_code',
+          'pdf417',
+          'qr_code',
+          'rm_qr_code',
+          'upc_a',
+          'upc_e',
+          'linear_codes',
+          'matrix_codes',
+          'unknown']} />
       </div>
       <Typography sx={{ wordBreak: 'break-word' }}>{decodedResults}</Typography>
       {qrCodeDescription && <QrCodeEdit qrCode={qrCodeDescription} />}
