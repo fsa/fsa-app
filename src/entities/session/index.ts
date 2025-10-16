693faf77bfc3db6027a8967fd7e9f975ca1da8ba
Sergei Fedotov <fsa@tavda.info>
2025-10-17 02:27:01 +0700

    Чистка кода

--- src/entities/session/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./model/sessionStore";
-export * from "./model/hooks/useLogin";
-export * from "./model/hooks/useLogout";
-export * from "./model/hooks/useApiQuery";
-export * from "./api/sessionApi";
